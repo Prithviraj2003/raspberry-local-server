8a48eb1d86a4fef6d1be9d15cca3b9d3fa623543
Prithvi <prithvirajindulkar2003@gmail.com>
2024-06-08 20:13:06 +0530

    env config

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
-console.log(process.env.DATABASE_URL);
-const sequelize = new Sequelize("postgresql://prithvi:Pr%409420268341@89.116.121.46:5432/college_security", {
+const dotenv = require("dotenv");
+dotenv.config();
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false
 });
--- config/grpc_setup.js
+++ config/grpc_setup.js
@@ -4,10 +4,11 @@ const path = require("path");
 const PROTO_PATH = path.join(__dirname, "../server.proto");
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);
 const proto = grpc.loadPackageDefinition(packageDefinition);
-
+const dotenv = require("dotenv");
+dotenv.config();
 // Create a gRPC client
 const client = new proto.EntryService(
-  "localhost:50051",
+  process.env.GRPC_SERVER_URL ,
   grpc.credentials.createInsecure()
 );
 module.exports = client;
