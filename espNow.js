5240216f0b145348beae61811ec59d59f85ca53c
Prithvi <prithvirajindulkar2003@gmail.com>
2024-06-24 03:09:54 +0530

    entry date model chnaged

--- espNow.js
+++ espNow.js
@@ -1,27 +1,32 @@
 const { SerialPortStream } = require("@serialport/stream");
 const { autoDetect } = require("@serialport/bindings-cpp");
 const { ReadlineParser } = require("@serialport/parser-readline");
-const {processCard} =require('./exports');
+const { processCard } = require("./exports");
 const DetectedBinding = autoDetect();
 
 // Create a new instance of SerialPortStream using the detected binding
 const port = new SerialPortStream({
-  path: '/dev/ttyS0',
+  path: "/dev/ttyS0",
   baudRate: 115200,
-  binding: DetectedBinding
+  binding: DetectedBinding,
 });
 
-const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+const parser = port.pipe(new ReadlineParser({ delimiter: "\n" }));
 
-port.on('open', () => {
-  console.log('Serial port is open');
+port.on("open", () => {
+  console.log("Serial port is open");
 });
 
-parser.on('data', data => {
-  const newData= JSON.parse(data);
-  processCard(newData.entryType,newData.token);
+parser.on("data", (data) => {
+  console.log(data);
+  try {
+    const newData = JSON.parse(data);
+    processCard(newData.entryType, newData.token);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-port.on('error', err => {
-  console.error('Error: ', err.message);
+port.on("error", (err) => {
+  console.error("Error: ", err.message);
 });
